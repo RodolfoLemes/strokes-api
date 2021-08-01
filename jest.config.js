810f6031d7b1939924f6b96b7ee813105ce81fd9
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-08-01 13:07:14 -0300

    :white_check_mark: test(jest): add controllers path to jest coverage

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,10 @@ module.exports = {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/modules/**/*.service.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/modules/**/*.service.ts',
+    '<rootDir>/src/modules/**/*.controller.ts',
+  ],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
