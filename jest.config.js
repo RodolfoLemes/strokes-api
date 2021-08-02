a75e4f83ae2510c255aa48e2372f7d38d0f361ef
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-08-02 19:46:33 -0300

    :globe_with_meridians: chore: refactor config files to accept seeds module

--- jest.config.js
+++ jest.config.js
@@ -27,7 +27,7 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/modules/seeds'],
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['text-summary', 'lcov'],
